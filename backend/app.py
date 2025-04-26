d971fad7ae1859f11b94da4368e7b193fdb6af69
WL <limweilun3838@gmail.com>
2025-04-26 21:17:15 +0800

    Added Planner API

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,13 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from routes.FoodItem import recipe
+from routes.Planner import planner
 
 app = Flask(__name__)
 CORS(app)  # allow requests from React
 
 app.register_blueprint(recipe, url_prefix='/api')
+app.register_blueprint(planner, url_prefix='/api')
 
 if __name__ == '__main__':
     app.run(debug=True)
