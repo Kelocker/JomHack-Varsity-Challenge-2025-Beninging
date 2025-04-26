feef2baaaf8346c339e59ce9726af07cb6ac6849
WL <limweilun3838@gmail.com>
2025-04-26 14:49:56 +0800

    Added API /api/recipe

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,11 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from routes.FoodItem import recipe
 
 app = Flask(__name__)
 CORS(app)  # allow requests from React
 
-@app.route('/api/hello')
-def hello():
-    return jsonify(message="Hello from Flask!")
+app.register_blueprint(recipe, url_prefix='/api')
 
 if __name__ == '__main__':
     app.run(debug=True)
